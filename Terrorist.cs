a378d3dbaa3e6b2b1b372c03fb1c91002b5e8086
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-05-16 10:28:37 +0300

    Add terrorist model and initial data

---
+++ Terrorist.cs
@@ -0,0 +1,9 @@
+public class Terrorist
+{
+    public string Name { get; set; }
+    public List<string> Weapons { get; set; }
+    public int Age { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string Affiliation { get; set; }
+}
